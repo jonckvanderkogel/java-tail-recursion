2da69522a9f35769107f4ce90a470fe3f1658321
jonckvanderkogel <jonckvanderkogel@gmail.com>
2018-12-15 00:40:57 +0100

    small generics improvement

--- src/main/java/tailrecursion/TailCalls.java
+++ src/main/java/tailrecursion/TailCalls.java
@@ -5,8 +5,8 @@ public class TailCalls {
         return nextCall;
     }
 
-    public static <T> TailCall done(final T value) {
-        return new TailCall() {
+    public static <T> TailCall<T> done(final T value) {
+        return new TailCall<T>() {
             @Override
             public boolean isComplete() {
                 return true;
