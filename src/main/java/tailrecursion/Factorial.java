239a4d7c8023104045f4b9cb70853febdf8048af
jonckvanderkogel <jonckvanderkogel@gmail.com>
2018-12-15 00:12:10 +0100

    small change for consistency

--- src/main/java/tailrecursion/Factorial.java
+++ src/main/java/tailrecursion/Factorial.java
@@ -1,11 +1,12 @@
 package tailrecursion;
 
 import static tailrecursion.TailCalls.call;
+import static tailrecursion.TailCalls.done;
 
 public class Factorial{
     public static TailCall<Long> factorialTailRec(final Long factorial, final int number) {
         if (number == 1)
-            return TailCalls.done(factorial);
+            return done(factorial);
         else
             return call(() -> factorialTailRec(factorial * number, number - 1));
     }
